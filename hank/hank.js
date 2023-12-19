80e42f52f4b14e23ab219ec951dbde0bcebb3e18
linjunzhan <86047175+linjunzhan@users.noreply.github.com>
2023-12-19 21:57:33 +0800

    1219上完課

---
+++ hank/hank.js
@@ -0,0 +1,10 @@
+ab();
+function ab(){var ab ="成功進入hank don\'t \"hello\"的網站";
+var b='我是甚麼'
+var c=Date();
+alert(ab+'ab參數是='+typeof(ab)+b+'標準時間'+c);
+document.writeln(ab);
+console.log(c);
+typeof(ab);
+    
+}
