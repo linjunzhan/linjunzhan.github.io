d2534ddc576cee38c5d2296e163ef9a94c13ea62
linjunzhan <86047175+linjunzhan@users.noreply.github.com>
2023-12-21 21:43:35 +0800

    新資料夾

---
+++ hank/projest/hellojs.js
@@ -0,0 +1,33 @@
+// var Hii = 'Hi 我是林均展'
+// var jslove = 'javascript是我最愛的語言XD'
+// var iwish = "我是這樣希望的"
+// document.writeln(jslove);
+// alert(Hii);
+// console.log(iwish);
+
+//document.writeln(thisCount2);
+//var thisCount ="2.5"*"2.0";
+//var thisCount1 ="2.5"+"2.0";
+//var thisCount2 = parseFloat(thisCount1)+2.0;
+//alert(thisCount2);
+
+//var x = y =1;
+//i=x++;
+//j=++y;
+//alert(i);
+//document.writeln(j);
+//console.log(i+","+j);
+
+//var msg =abc("你好嗎?");
+//function abc(x){return x+"我收到字串了"}
+//document.writeln(msg);
+
+
+var mag = ab(4);
+
+function ab(x) {
+    if (x > 10) return "我就結婚";
+    else if (x > 5) return "我再想想看";
+    else return "就算了";
+}
+document.writeln(mag);
