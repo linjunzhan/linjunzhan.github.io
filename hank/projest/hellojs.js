c390611626447d8a0069d8e65c315b8191edd5d0
linjunzhan <86047175+linjunzhan@users.noreply.github.com>
2023-12-26 21:47:48 +0800

    1226

--- hank/projest/hellojs.js
+++ hank/projest/hellojs.js
@@ -23,11 +23,60 @@
 //document.writeln(msg);
 
 
-var mag = ab(8);
+//var mag = ab(8);
+//
+//function ab(x) {
+//    if (x > 10) return "我就結婚";
+//    else if (x > 5) return "我再想想看";
+//    else return "就算了";
+//}
+//document.writeln(mag);
 
-function ab(x) {
-    if (x > 10) return "我就結婚";
-    else if (x > 5) return "我再想想看";
-    else return "就算了";
+//
+//for(i=0;i<=50;i++){
+//    if(i%2==0)//偶數判別方式
+//    if(i%3!=0)
+//    {
+//    document.writeln(i);
+//}
+//}
+//
+
+
+//for (i = 0; i <= 50; i++) {
+//    if (i % 2 != 0)//奇數判別方式
+//
+//    {
+//        document.writeln(i);
+//
+//    }
+//}
+
+
+//var i =0
+//while (i <= 50) {
+//    if(i %2 !=0 )
+//        if(i%7==0)
+////        if(i%7 == 0)
+////            if(i%3 !=0)
+////                if(i%5 !=0)
+//    
+//    document.writeln(i);
+//    i++; 
+//}
+
+
+
+function validateForm()
+
+{
+
+    var username = document.getElementById("username").value;
+    alert("你的名字是"+username);
+    var email = document.getElementById("email").value;
+    alert("你的email是"+email);
+     var phone = document.getElementById("phone").value;
+    alert("你的phone是"+phone);
+
+
 }
-document.writeln(mag);
