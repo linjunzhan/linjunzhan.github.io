c3141755a0e995c2a61c72ede68855674fdbfbd0
linjunzhan <86047175+linjunzhan@users.noreply.github.com>
2023-12-28 17:39:50 +0800

    1228

--- hank/projest/hellojs.js
+++ hank/projest/hellojs.js
@@ -23,34 +23,42 @@
 //document.writeln(msg);
 
 
-//var mag = ab(8);
-//
-//function ab(x) {
-//    if (x > 10) return "我就結婚";
-//    else if (x > 5) return "我再想想看";
-//    else return "就算了";
-//}
-//document.writeln(mag);
-
-//
-//for(i=0;i<=50;i++){
-//    if(i%2==0)//偶數判別方式
-//    if(i%3!=0)
-//    {
-//    document.writeln(i);
-//}
-//}
-//
 
 
-//for (i = 0; i <= 50; i++) {
-//    if (i % 2 != 0)//奇數判別方式
-//
-//    {
-//        document.writeln(i);
-//
-//    }
-//}
+function getmerry(monny = "輸入金額") {
+    if (monny >= 10) return "我就結婚";
+    else if (monny >= 5) return "我在想想看";
+    else return "那就算了"
+}
+
+var gg = getmerry(10);
+//console.log(gg);
+
+
+
+function ooo(os, oe) {
+    for (i = os; i <= oe; i++) {
+        if (i % 2 == 0) {
+            console.log(i);
+        }
+    }
+} //偶數判別方式
+
+
+
+
+function jjj(js, je) {
+    for (var i = js; i <= je; i++) {
+        if (i % 2 != 0) {
+            console.log(i);
+        }
+    }
+
+} //奇數判別方式
+
+
+//ooo(0, 50);//呼叫函數
+
 
 
 //var i =0
@@ -61,22 +69,67 @@
 ////            if(i%3 !=0)
 ////                if(i%5 !=0)
 //    
-//    document.writeln(i);
+//    console.log(i);
+////    document.writeln(i);
+////        alert(i);
 //    i++; 
 //}
 
 
-
-function validateForm()
-
-{
-
-    var username = document.getElementById("username").value;
-    alert("你的名字是"+username);
-    var email = document.getElementById("email").value;
-    alert("你的email是"+email);
-     var phone = document.getElementById("phone").value;
-    alert("你的phone是"+phone);
-
-
+function showMsg(username){
+    console.log('HI + 'username' +,wlcome to my js function');
 }
+showMsg(username);
+
+
+
+//function isPrime(n)
+//{
+//    if (n <= 1) 
+//    {
+//        return false;
+//    }
+//
+//    for (var i = 2; i <= Math.sqrt(n); i++) 
+//    {
+//        if (n % i === 0) 
+//        {
+//            return false;
+//        }
+//    }
+//
+//    return true;
+//}
+//
+//function main() {
+//    for (var i = 2; i <= 50; i++) {
+//        if (isPrime(i)) {
+//            //      console.log(i);
+//            document.writeln(i);
+//        }
+//    }
+//}
+//
+//main();
+
+
+
+
+
+
+
+
+
+//function validateForm()
+//
+//{
+//
+//    var username = document.getElementById("username").value;
+//    alert("你的名字是"+username);
+//    var email = document.getElementById("email").value;
+//    alert("你的email是"+email);
+//     var phone = document.getElementById("phone").value;
+//    alert("你的phone是"+phone);
+//
+//
+//}
